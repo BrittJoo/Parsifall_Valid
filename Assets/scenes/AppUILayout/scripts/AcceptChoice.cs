7bef56b3776c99063de6d0d1cb24030e23caae8d
BrittJoo <britt-joosten@hotmail.com>
2018-06-12 12:05:04 +0200

    Uploading the project

---
+++ Assets/scenes/AppUILayout/scripts/AcceptChoice.cs
@@ -0,0 +1,135 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class AcceptChoice : MonoBehaviour {
+
+    public string[] statementText;//= new string[] {"Statement1", "Statement2", "Statement3"};
+    public bool overMouse;
+    private paddle paddle;
+    public Text StatementTextObject;
+  //  List<int> statement = new List<int>();
+    int currentStatement;
+    int lastStatement;
+    int snapPoint;
+    int currentTextIndex = 0;
+    bool canClickNext = true;
+    //  int currentChar;
+    //  int character;
+
+    //private var wave1 : List.<String> = new List.<String>();
+    // private int waveArray = List.< List.< String > > = new List.< List.< String > > ();
+
+    [SerializeField] List<int> statement = new List<int>();
+    [SerializeField] List<int> character = new List<int>();
+    [SerializeField] List<int> answers = new List<int>();
+    //  List<List<int>> character = new List<List<int>>();
+    //List<List<int>> statement = new List<List<int>>();
+    //public float[] statement;
+    //public float[] statementPoints;
+
+    // Use this for initialization
+    void Start()
+    {
+        paddle = FindObjectOfType<paddle>();
+        int indexChar = character.IndexOf(120);
+       // List.IndexOf(character);
+
+
+        statement.Add(1);
+        statement.Add(2);
+        statement.Add(3);
+        statement.Add(4);
+        statement.Add(5);
+        statement.Add(6);
+        statement.Add(7);
+        statement.Add(8);
+        statement.Add(9);
+        statement.Add(10);
+        statement.Add(11);
+        statement.Add(12);
+        statement.Add(13);
+        statement.Add(14);
+        statement.Add(15);
+        statement.Add(16);
+        statement.Add(17);
+        statement.Add(18);
+        statement.Add(19);
+        statement.Add(20);
+
+        foreach (int o in statement)
+        {
+            for (int i = 0; i< 6; i++)
+            {
+                character.Add(i);
+            }
+        }
+
+        foreach (int p in character)
+        {
+            for (int i = 0; i< 4; i++)
+            {
+                answers.Add(i);
+            }
+        }
+        currentStatement = 1;
+        lastStatement = 1;
+        // xml, 
+    }
+	
+	// Update is called once per frame
+	void Update()
+    {
+       
+    }
+
+   public void ClickingTask()
+    {
+        if (canClickNext)
+        {
+            print("op knop geklikt");
+            //check which square
+            lastStatement = currentStatement;
+            //StatementTextObject set text to array.
+            currentStatement++;
+            StatementTextObject.text = statementText[currentTextIndex];
+            Debug.Log(statementText[currentTextIndex]);
+            Debug.Log("de currentTextIndex vastgesteld");
+            currentTextIndex++;
+            Debug.Log(currentTextIndex);
+        }
+
+        if (currentTextIndex == 5)
+        {
+            //break
+            Debug.Log("vijfde statement hier");
+            canClickNext = false;
+            Debug.Log(canClickNext);
+            this.gameObject.GetComponent<Image>().enabled = false;
+            StartCoroutine(Wait(500));
+            this.gameObject.GetComponent<Image>().enabled = true;
+            canClickNext = true;
+        }
+        // kijk welke catagorie is geklikt
+        // sla de punten op
+        // ga naar de volgende stelling
+    }
+
+   /* void OnMouseOver()
+    {
+        overMouse = true;
+    }
+
+    private void OnMouseExit()
+    {
+        overMouse = false;
+    }*/
+
+    public IEnumerator Wait(float waitTime)
+    {
+        print("in coroutine");
+        yield return new WaitForSeconds(waitTime);
+        print("beyond coroutine");
+    }
+}
---
+++ Assets/scenes/database classes/Database.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Mono.Data.Sqlite;
+using System.Data;
+using System;
+
+
+
+public class Database : MonoBehaviour {
+
+
+    
+
+    void Start () {
+        
+        string conn = "URI=file:" + Application.dataPath + "/Plugins/Users.s3db";
+        IDbConnection dbconn;
+        dbconn = (IDbConnection)new SqliteConnection(conn);
+        dbconn.Open(); //Open connection to the database.
+        IDbCommand dbcmd = dbconn.CreateCommand();
+        string sqlQuery = "SELECT id, Voornaam, Achternaam, Klas FROM Usersinfo";/*"SELECT value,name, randomSequence " + "FROM Usersinfo";*/
+        dbcmd.CommandText = sqlQuery;
+        IDataReader reader = dbcmd.ExecuteReader();
+        while (reader.Read())
+        {
+            int id = reader.GetInt32(0);
+            string Voornaam = reader.GetString(1);
+            string Achternaam = reader.GetString(2);
+            int Klas = reader.GetInt32(3);
+            Debug.Log("value = " + id + "Voornaam = " + Voornaam + "  achternaam = " + Achternaam + "  klas = " + Klas);
+        }
+        reader.Close();
+        reader = null;
+        dbcmd.Dispose();
+        dbcmd = null;
+        dbconn.Close();
+        dbconn = null;
+    }
+}
