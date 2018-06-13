386c5de2102443db60f0feac0ccbaba79d6c4dc3
Jip Sterk <JipSterk@users.noreply.github.com>
2018-06-13 11:12:36 +0200

    Added CardDrag

---
+++ Assets/DropZone.cs
@@ -0,0 +1,33 @@
+﻿using UnityEngine;
+using UnityEngine.EventSystems;
+
+namespace Parsifall
+{
+    public class DropZone : MonoBehaviour, IDropHandler, IPointerEnterHandler, IPointerExitHandler
+    {
+        public void OnDrop(PointerEventData eventData)
+        {
+            var answer = eventData.pointerDrag.GetComponent<Answer>();
+            if (!answer) return;
+            answer.Drop(transform);
+        }
+
+        public void OnPointerEnter(PointerEventData eventData)
+        {
+            if (!eventData.pointerDrag) return;
+
+            var answer = eventData.pointerDrag.GetComponent<Answer>();
+            if (!answer) return;
+            answer.EnterDrop(transform);
+        }
+
+        public void OnPointerExit(PointerEventData eventData)
+        {
+            if (!eventData.pointerDrag) return;
+
+            var answer = eventData.pointerDrag.GetComponent<Answer>();
+            if (!answer) return;
+            answer.ExitDrop(transform);
+        }
+    }
+}
