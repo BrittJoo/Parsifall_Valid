99da9698fb7813d080d99b6950d0b67f7c0bd964
Jip Sterk <JipSterk@users.noreply.github.com>
2018-06-13 14:05:34 +0200

    Updated Questions

---
+++ Assets/Scripts/ChoiceMaking.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+
+namespace Parsifall
+{
+    public class ChoiceMaking : MonoBehaviour
+    {
+        [SerializeField] private Text _statement;
+
+        private Options _options;
+
+        public void SetStatement(float index)
+        {
+            _options = (Options) (int) index;
+        }
+
+        public void Choose()
+        {
+            var statement = _statement.text;
+
+            GameManager.Instance.Choose(_options, statement);
+        }
+    }
+}
--- Assets/scenes/database classes/Database.cs
+++ Assets/scenes/database classes/Database.cs
@@ -1,40 +1,31 @@
-﻿using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
+﻿using UnityEngine;
 using Mono.Data.Sqlite;
-using System.Data;
-using System;
 
+public class Database : MonoBehaviour
+{
+    private void Start()
+    {
+        var conn = string.Format("URI=file:${0}/Plugins/Users.s3db", Application.dataPath);
+        var connection = new SqliteConnection(conn);
 
+        connection.Open();
 
-public class Database : MonoBehaviour {
-
-
-    
-
-    void Start () {
-        
-        string conn = "URI=file:" + Application.dataPath + "/Plugins/Users.s3db";
-        IDbConnection dbconn;
-        dbconn = (IDbConnection)new SqliteConnection(conn);
-        dbconn.Open(); //Open connection to the database.
-        IDbCommand dbcmd = dbconn.CreateCommand();
-        string sqlQuery = "SELECT id, Voornaam, Achternaam, Klas FROM Usersinfo";/*"SELECT value,name, randomSequence " + "FROM Usersinfo";*/
-        dbcmd.CommandText = sqlQuery;
-        IDataReader reader = dbcmd.ExecuteReader();
-        while (reader.Read())
+        using (var command = new SqliteCommand(@"SELECT id, Voornaam, Achternaam, Klas FROM Usersinfo"))
         {
-            int id = reader.GetInt32(0);
-            string Voornaam = reader.GetString(1);
-            string Achternaam = reader.GetString(2);
-            int Klas = reader.GetInt32(3);
-            Debug.Log("value = " + id + "Voornaam = " + Voornaam + "  achternaam = " + Achternaam + "  klas = " + Klas);
+            var reader = command.ExecuteReader();
+            while (reader.Read())
+            {
+                var id = reader.GetInt32(0);
+                var voorNaam = reader.GetString(1);
+                var achterNaam = reader.GetString(2);
+                var klas = reader.GetInt32(3);
+                Debug.Log("value = " + id + "Voornaam = " + voorNaam + "  achternaam = " + achterNaam + "  klas = " +
+                          klas);
+            }
+
+            reader.Close();
         }
-        reader.Close();
-        reader = null;
-        dbcmd.Dispose();
-        dbcmd = null;
-        dbconn.Close();
-        dbconn = null;
+
+        connection.Close();
     }
 }
